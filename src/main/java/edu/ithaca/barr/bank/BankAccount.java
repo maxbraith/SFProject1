3c8d8a1c2c124bba65680dd429c0b51b95362f4c
mattweil11 <mattweil11@gmail.com>
2023-02-13 11:40:37 -0500

    Copied in Bank Account and tests

--- src/main/java/edu/ithaca/barr/bank/BankAccount.java
+++ src/main/java/edu/ithaca/barr/bank/BankAccount.java
@@ -6,30 +6,52 @@ public class BankAccount {
     private double balance;
 
     /**
-     * @throws IllegalArgumentException if email is invalid
+     * Constructor for BankAccount
+     * @throws IllegalArgumentException if email is invalid 
      */
     public BankAccount(String email, double startingBalance){
+        //make sure email is valid
         if (isEmailValid(email)){
             this.email = email;
             this.balance = startingBalance;
-        }
-        else {
+        }else{
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
+        }
+        //make sure startBalnce is Valid
+        if(isAmountValid(startingBalance)){
+            this.balance = startingBalance;
+        }else{
+            throw new IllegalArgumentException("Amount is invalid, cannot create account");
         }
     }
 
+    /**
+     * Getter for balance
+     * @return the balance of the bank account
+     */
     public double getBalance(){
         return balance;
     }
 
+    /**
+     * Getter for email
+     * @return email
+     */
     public String getEmail(){
         return email;
     }
 
     /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
+     * If the amount is negative:
+     * @throws InsufficientFundsException
+     * If the amount is larger then balance:
+     * @throws IllegalArgumentException
      */
     public void withdraw (double amount) throws InsufficientFundsException{
+        if(!isAmountValid(amount)){
+            throw new IllegalArgumentException("Amount cannot be negative");
+        }
         if (amount <= balance){
             balance -= amount;
         }
@@ -38,13 +60,102 @@ public class BankAccount {
         }
     }
 
-
-    public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
+    /**
+     * Checks to see if an email for the bank account is valid or not
+     * @param takes an email
+     * @return a boolean that says if an email is valid or not 
+     */
+    public static boolean isEmailValid(String email){ //this method was made by my partner: Max, the comments are my own though
+        if(email.length() < 5){ //checks to make sure email is valid length
             return false;
         }
-        else {
+        else if(email.indexOf('@') <= 0){ //checks to make sure @ is not the first character
+            return false;
+        }
+        else if(email.lastIndexOf('.') >= email.length()-2 || email.indexOf('.') == 0){ //checks to make sure there are at least 2 characters after the . in the domain
+            return false;
+        }
+        else if(Character.isLetter(email.charAt(email.indexOf('@')-1)) == false){ //checks to make sure email has a @ sign
+            return false;
+        }
+        else if(email.charAt(email.indexOf('.')+1) == '.' || email.charAt(email.lastIndexOf('.')-1) == '.'){ //checks to make sure . is not the first nor last character 
+            return false;
+        }
+        else if(email.contains("#")){ //checks to make sure email does not contain #
+            return false;
+        }
+        else if(Character.isLetter(email.charAt(email.length()-1))==false){ //checks to make sure first character is a letter
+            return false;
+        }
+        else if(email.lastIndexOf('.') < email.indexOf('@')){ //checks to make sure . is after the @
+            return false;
+        }
+        else if(Character.isLetter(email.charAt(email.indexOf('.')+1)) == false){ //checks to make sure there is another letter after a .
+            return false;
+        }
+        else if(Character.isLetter(email.charAt(email.indexOf('_')+1))==false){//checks to make sure there is another letter after a _
+            return false;
+        }
+        else if(Character.isLetter(email.charAt(email.indexOf('-')+1))==false){ //checks to make sure there is another letter after a -
+            return false;
+        }
+        else if(email.contains(" ")) {//checks for whitespace in the email
+            return false;
+        }else{
             return true;
+        }
+    }
+
+    /**
+     * If the amount is positive and has 2 or less decimal places: returns true
+     * If the amount is negative or the amount has more than 2 decimal places: returns false
+     * @param amount, the amount of money being checked
+     * @return boolean of valid or invalid amount
+     */
+    public static boolean isAmountValid(double amount){
+        //Check if amount is less than 0.00
+        if(amount < 0.0){
+            return false;
+        }else{
+            //Check to make sure amount has less then 2 decimal places
+            String temp = Double.toString(amount); //put into string to check amount of places past decimal point
+            int decimalNumber = temp.indexOf('.'); //get the position in the string of .
+            if((temp.length()-decimalNumber -1) > 2){ //gets the number of decimal places and checks if more than 2
+                return false;
+            }else{
+                return true;
+            }
+        }
+    }
+
+    /**
+     * Adds money to the bank account
+     * @param amount, the amount of money to add to the bank account
+     * @throws IllegalArgumentException if amount is invalid
+     * @throws IllegalAccessException
+     */
+    public void deposit(double amount) throws IllegalArgumentException{
+        if(isAmountValid(amount)){ 
+            balance = balance + amount; // if amount is valid, adds it into the account
+        }else{
+            throw new IllegalArgumentException("Amount is invalid so we can not do this desired deposit into your bank account.");
+        }
+    }
+
+    /**
+     * Transfers money from one account to another account if the transfer amount is valid
+     * @param transferBankAccount different bank account to transfer the money to
+     * @param amount the amount of money to transfer to the new bank account
+     * @throws IllegalArgumentException
+     * @throws InsufficientFundsException
+     */
+    public void transfer(BankAccount transferBankAccount, double amount) throws IllegalArgumentException, InsufficientFundsException{
+        if(!isAmountValid(amount)|| transferBankAccount == null){ //Checks to make sure amount is valid and that another bank account exists
+            throw new IllegalArgumentException(); 
+        }else{
+            //gets money from one account and adds it to the other account
+            this.withdraw(amount);
+            transferBankAccount.deposit(amount);
         }
     }
 }
--- src/test/java/edu/ithaca/barr/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/barr/bank/BankAccountTest.java
@@ -1,5 +1,4 @@
 package edu.ithaca.barr.bank;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +10,13 @@ class BankAccountTest {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
 
         assertEquals(200, bankAccount.getBalance(), 0.001);
+
+        //Equivalence class: have money in bank account account
+        assertEquals(200, bankAccount.getBalance());
+        //Equivalence class: have no money in bank account
+        BankAccount bankAccount1 = new BankAccount("apple@pie.com", 0);
+        assertEquals(0, bankAccount1.getBalance());
+
     }
 
     @Test
@@ -20,6 +26,29 @@ class BankAccountTest {
 
         assertEquals(100, bankAccount.getBalance(), 0.001);
         assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300));
+    
+        //Equivalence class: amount larger than balance
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(150));
+        
+        //Equivalence class: amount is smaller than balance
+        bankAccount.withdraw(50);
+        assertEquals(50, bankAccount.getBalance());
+
+        //Equivalence class: amount is negative
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.withdraw(-50));
+
+        //Boundary case: amount is zero
+        bankAccount.withdraw(0);
+        assertEquals(50, bankAccount.getBalance());
+
+        //Boundary case: amount is equal to balance
+        bankAccount.withdraw(bankAccount.getBalance());
+        assertEquals(0, bankAccount.getBalance());
+
+        //Equivalence case: amount has decimal with more then 2 places
+        BankAccount bankAccount2 = new BankAccount("test@mail.com", 50);
+        assertThrows(IllegalArgumentException.class, ()->bankAccount2.withdraw(0.001));
+
     }
 
     @Test
@@ -27,7 +56,112 @@ class BankAccountTest {
         assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
         assertFalse( BankAccount.isEmailValid(""));         // empty string
 
-        
+        //Tests added for Task 01-02
+        //Prefix tests
+        assertTrue(BankAccount.isEmailValid( "abc-d@mail.com")); //Equivalence class: Valid email -  dash within prefix is valid
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.com")); //Equivalence class: Valid email - period within prefix is valid
+        assertTrue(BankAccount.isEmailValid("abcdefg@mail.com")); //Equivalence class: Valid email - normal email with period and just letters is valid
+        assertTrue(BankAccount.isEmailValid("abc_sample@mail.com")); //Equivalence class: Valid email - underscore in the middle of the prefix is valid
+
+        assertFalse(BankAccount.isEmailValid("abcd-@mail.com")); //Equivalence class: Invalid email - prefix cannot end in a dash
+        assertFalse(BankAccount.isEmailValid("abc..sample@mail.com")); //Equivalence class: Invalid email - consecutive periods not allowed in prefix
+        assertFalse(BankAccount.isEmailValid(".sample@mail.com")); //Equivalence class: Invalid email - prefix must begin with letter
+        assertFalse(BankAccount.isEmailValid("abc#sample@mail.com")); //Equivalence class: Invalid email - Hash mark not allowed in prefix
+
+        //Added after code review
+        assertFalse(BankAccount.isEmailValid("test @gmail.com")); //Equivalence class: Invalid email - contains whitespace in prefix
+
+
+        //Domain tests
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.cc")); //Equivalence class: Valid email - domain with two characters at the end is valid
+        assertTrue(BankAccount.isEmailValid("abc.def@mail-archive.com")); //Equivalence class: Valid email - domain with - is valid as long as it is not the last character
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.org")); //Equivalence class: Valid email - Domain must have at least two characters
+        assertTrue(BankAccount.isEmailValid("abc.def@mail.com"));  //Equivalence class: Valid email - Domain must have at least two characters
+
+        assertFalse(BankAccount.isEmailValid("abc.sample@mail.c")); //Equivalence class: Invalid email - last section of domain must have at least two characters
+        assertFalse(BankAccount.isEmailValid("abc.sample@mail.mail#archive.com")); //Equivalence class: Invalid email - Domain must not have special character #
+        assertFalse(BankAccount.isEmailValid("abc.sample@mail")); //Equivalence class: Invalid email - last section of domain must have at least two characters
+        assertFalse(BankAccount.isEmailValid("abc.sample@mail..com")); //Equivalence class: Invalid email - last section of domain can not have consecutive periods
+
+        //Added after code review
+        assertFalse(BankAccount.isEmailValid("test@gma il.com")); //Equivalence class: Invalid email - contains whitespace in domain
+
+        //Tests added for Task 01-03
+        //Prefix tests
+        assertFalse(BankAccount.isEmailValid("#sample@sample.com")); //Equivalence case: invalid character at beginning of prefix
+        assertFalse(BankAccount.isEmailValid("sam#le@sample.com")); //Equivalence case: invalid character in middle of prefix
+        assertFalse(BankAccount.isEmailValid("sample#@sample.com")); //Equivalence case:invalid character at end of prefix
+        assertTrue(BankAccount.isEmailValid("sa-mple@sample.com"));  //Equivalence case: dash followed by a letter or number in prefix
+        assertFalse(BankAccount.isEmailValid("sample-@sample.com")); //Equivalence case: dash, dot, or underscore not followed by a letter or number in prefix
+        assertFalse(BankAccount.isEmailValid("samp-.-le@sample.com")); //Equivalence case: more than 2 dots, dashes, underscores in a row in prefix
+        assertFalse(BankAccount.isEmailValid("s__ample@sample.com"));  //Boundary case: 2 dots, dashes, or underscores in a row in prefix
+
+        //Domain tests
+        assertFalse(BankAccount.isEmailValid("sample@sample.c")); //Equivalence case: less than two characters in last portion of domain
+        assertTrue(BankAccount.isEmailValid("sample@sample.co")); //Boundary case: 2 characters in last portion of domain
+        assertTrue(BankAccount.isEmailValid("sample@sample.com")); //Equivalence case: more than two characters in last portion of domain
+        assertFalse(BankAccount.isEmailValid("sample@#sample.com")); //Equivalence case: invalid character at beginning of domain
+        assertFalse(BankAccount.isEmailValid("sample@te#st.com")); //Equivalence case: invalid character in middle of domain
+        assertFalse(BankAccount.isEmailValid("sample@sample.com#")); //Equivalence case: invalid character at end of domain
+        assertFalse(BankAccount.isEmailValid("sample@sample.com-")); //Equivalence case: dash not followed by a letter or number in domain
+        assertFalse(BankAccount.isEmailValid("sample@sa---mple.com")); //Equivalence case: more than 2 dashes in a row in domain
+        assertFalse(BankAccount.isEmailValid("sample@sample.c--om"));  //Boundary case: 2 dashes in a row in domain
+
+    }
+
+    @Test
+    void isAmountValidTest(){
+        //Equivalence Case: Amount is positive
+        assertTrue(BankAccount.isAmountValid(10));
+        //Equivalence Case: Amount is negative
+        assertFalse(BankAccount.isAmountValid(-10));
+        //Equivalence Case: Amount is positive and has two decimal places
+        assertTrue(BankAccount.isAmountValid(10.01));
+        //Equivalence Case: Amount is positive and has more then two decimal places
+        assertFalse(BankAccount.isAmountValid(10.001));
+    }
+
+    @Test
+    void depositTest(){
+        BankAccount bankAccount = new BankAccount("test@mail.com", 100);
+
+        //Equivalence class: amount is positive
+        bankAccount.deposit(100);
+        assertEquals(200, bankAccount.getBalance());
+        //Equivalence class: amount is zero - border case
+        bankAccount.deposit(0);
+        assertEquals(200, bankAccount.getBalance());
+        //Equivalence class: amount is negative
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-5));
+        //Equivalence class: amount has more than 2 decimals
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-5.005));
+        //Equivalence class: amount has less than 2 decimals
+        bankAccount.deposit(10.50);
+        assertEquals(210.50, bankAccount.getBalance());
+    }
+
+    @Test
+    void transferTest() throws InsufficientFundsException{
+        //make two bank accounts, so we can test if the money in one can be transferred to another
+        BankAccount bankAccount = new BankAccount("test@mail.com", 100);
+        BankAccount transferAccount = new BankAccount("a@b.com", 0);
+
+        //Equivalence class: transfer less money then account balance into the other account
+        bankAccount.transfer(transferAccount,50);
+        assertEquals(50, bankAccount.getBalance());
+        assertEquals(50, transferAccount.getBalance());
+        //Equivalence class: transfer more money then account balance into the other account
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.transfer(transferAccount, 500));
+        //Equivalence class: transfer negative amount of money
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.transfer(transferAccount, -50));
+        //Equivalence class: transfer an invalid amount of money, more then 2 decimal places
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.transfer(transferAccount, 50.001));
+        //Equivalence class: transfer bank account does not exist
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.transfer(null, 20));
+        //Equivalence class: transfer the entire bank account to the other bank account
+        bankAccount.transfer(transferAccount, 50);
+        assertEquals(0, bankAccount.getBalance());
+        assertEquals(100, transferAccount.getBalance());
     }
 
     @Test
@@ -38,6 +172,11 @@ class BankAccountTest {
         assertEquals(200, bankAccount.getBalance(), 0.001);
         //check for exception thrown correctly
         assertThrows(IllegalArgumentException.class, ()-> new BankAccount("", 100));
+
+        //Equivalence class: negative amount in bank account
+        assertThrows(IllegalArgumentException.class, ()-> new BankAccount("test@mail.com", -10));
+        //Equivalence class: amount has more then 2 decimal places
+        assertThrows(IllegalArgumentException.class, ()-> new BankAccount("test@mail.com", 10.001));
     }
 
 }
